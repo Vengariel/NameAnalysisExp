90ef0e7768ebba5dfac57d17dafca043a12f9249
Marco Mendieta <ardenteveritate@live.com>
2019-05-27 12:26:25 -0500

    added duplicates removal by curp / full name

--- NameAnalysis/NameExtractor.cs
+++ NameAnalysis/NameExtractor.cs
@@ -4,20 +4,21 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
 using System.IO;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace NameAnalysis
 {
     public static class NameExtractor
     {
-
-        public static List<PersonName> GetPersonNameList(string csvPath, char cSeparator, int iColumnNumber)
+        public static List<PersonName> GetPersonNameList(string csvPath, char cSeparator, int iColumnNumber, bool useUniqueCURP =false)
         {
             List<PersonName> personNameList = null;
 
             if (File.Exists(csvPath))
             {
                 personNameList = new List<PersonName>();
-                
+
                 using (StreamReader sr = new StreamReader(csvPath))
                 {
                     if (!sr.EndOfStream)
@@ -30,15 +31,16 @@ namespace NameAnalysis
                         string row = sr.ReadLine();
                         string[] sWords;
                         if (!string.IsNullOrEmpty(row))
-                        {                 
+                        {
                             sWords = row.Split(cSeparator);
                             if (sWords.Length == iColumnNumber)
                             {
                                 PersonName person = new PersonName()
                                 {
-                                    FirstName = sWords[(int) ColumnsEnum.Name],
-                                    FirstLastName = sWords[(int) ColumnsEnum.FirstLastName],
-                                    SecondLastName = sWords[(int) ColumnsEnum.SecondLastName]
+                                    FirstName = sWords[(int)ColumnsEnum.Name],
+                                    FirstLastName = sWords[(int)ColumnsEnum.FirstLastName],
+                                    SecondLastName = sWords[(int)ColumnsEnum.SecondLastName],
+                                    CURP = sWords[(int)ColumnsEnum.CURP]
                                 };
                                 if (person.FirstName == null)
                                 {
@@ -50,7 +52,13 @@ namespace NameAnalysis
                     }
                 }
             }
-            return personNameList;
+            return useUniqueCURP ? GetUniquePersonName( personNameList) : personNameList;
+        }
+
+        private static List<PersonName> GetUniquePersonName(List<PersonName> personNames)
+        {
+            var curpGroups = personNames.GroupBy(p => p, new PersonComparer());
+            return curpGroups.Select(g => g.First()).ToList();
         }
     }
 }
---
+++ NameAnalysis/PersonComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace NameAnalysis
+{
+    public class PersonComparer : IEqualityComparer<PersonName>
+    {
+        public bool Equals(PersonName x, PersonName y)
+        {
+            return string.Compare(x.CURP, y.CURP, CultureInfo.CurrentCulture,
+                       CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace) == 0;
+        }
+
+        public int GetHashCode(PersonName obj)
+        {
+            return (string.IsNullOrEmpty(obj.CURP) ? obj.FullLastName.GetHashCode() : obj.CURP.GetHashCode());
+        }
+    }
+}
